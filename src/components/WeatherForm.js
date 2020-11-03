b489acd04f27fb329bb2b144ccb2892bd8be067a
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-11-03 09:23:52 -0600

    add components

---
+++ src/components/WeatherForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function WeatherForm(props) {
+
+  return (
+    <form>
+      <label>Location</label>
+      <input type="text"></input>
+      <button onClick={}>Submit</button>
+    </form>
+  );
+}
+
+export default WeatherForm;
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <h1>Weather</h1>;
+}
+
+export default Header;

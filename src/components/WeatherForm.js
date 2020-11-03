c4fc49c6630a1c2d006d96da517f7b5f8629c39b
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-11-03 09:26:01 -0600

    use class

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function WeatherForm(props) {
-
-  return (
-    <form>
-      <label>Location</label>
-      <input type="text"></input>
-      <button onClick={}>Submit</button>
-    </form>
-  );
+class WeatherForm extends React.Component {
+  render() {
+    return (
+        <form>
+          <label>Location</label>
+          <input type="text"></input>
+          <button onClick={}>Submit</button>
+        </form>
+    );
+  }
 }
 
 export default WeatherForm;
